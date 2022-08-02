e27c3b06fd9b52b55126a988cec425af79c52dff
junwon1997 <junwon20170339@gmail.com>
2022-08-02 23:37:48 +0900

    아이디 찾기, 비밀번호 찾기 주석 & 전체적인 페이지 생성

--- mmm_client/src/app.jsx
+++ mmm_client/src/app.jsx
@@ -4,12 +4,14 @@ import { Route, Routes } from "react-router-dom";
 /* page들 import */
 import Login from "./components/login";
 import Signup from "./components/signup";
+import Main from "./components/main";
 
 function App() {
   return (
     <>
       <Routes>
-        <Route exact path="/" element={<Login />}></Route>
+        <Route exact path="/" element={<Main />}></Route>
+        <Route exact path="/login" element={<Login />}></Route>
         <Route exact path="/signup" element={<Signup />}></Route>
       </Routes>
     </>
