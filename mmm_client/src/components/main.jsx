9c181cd329f1e88dd6efa8577071e62ced50b4f1
junwon1997 <junwon20170339@gmail.com>
2022-08-10 17:08:57 +0900

    설문조사 게시 화면 제목 컴포넌트

--- mmm_client/src/components/main.jsx
+++ mmm_client/src/components/main.jsx
@@ -10,6 +10,24 @@ const Main = () => {
       <Link to="/login" style={{ textDecoration: "none", cursor: "pointer" }}>
         <div>로그인 화면 가기</div>
       </Link>
+      <div className="surveyPostTitleBox">
+        <div className="surveyPostTitleInput">
+          <input
+            id="surveyPostTitleInputId"
+            type="text"
+            name="id"
+            placeholder="설문조사의 제목을 입력하세요."
+          />
+        </div>
+        <div className="surveyPostDescriptionInput">
+          <input
+            id="surveyPostDescriptionInputId"
+            type="text"
+            name="id"
+            placeholder="설문조사의 설명을 입력하세요."
+          />
+        </div>
+      </div>
     </>
   );
 };
