ed90df840eaa7c120842db212cb6d7e8025db01e
seyxxn <lucy9480@gmail.com>
2022-08-14 15:20:14 +0900

    마이페이지 메뉴 목록 수정 (공지사항, 자주묻는질문)

--- mmm_client/src/components/myPageListContainer.jsx
+++ mmm_client/src/components/myPageListContainer.jsx
@@ -9,7 +9,8 @@ const MyPageListContainer = () => {
         <div className="myPageListItems">내 설문조사</div>
         <div className="myPageListItems">포인트</div>
         <div className="myPageListItems">쿠폰</div>
-        <div className="myPageListItems">설정</div>
+        <div className="myPageListItems">공지사항</div>
+        <div className="myPageListItems">자주 묻는 질문</div>
       </div>
       <div className="myPageListContainerBottom">
         <button className="myPageLogoutBnt">로그아웃</button>
