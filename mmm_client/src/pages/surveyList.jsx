d111383b19a4d472ce696f6bfbcd43e4f71b0f19
seyxxn <lucy9480@gmail.com>
2022-08-16 14:55:14 +0900

    설문조사 대상 선택 시 dropBox 변화 완성

--- mmm_client/src/pages/surveyList.jsx
+++ mmm_client/src/pages/surveyList.jsx
@@ -1,13 +1,12 @@
 import React from "react";
 import Navbar from "../components/navbar";
 import { useState } from "react";
-import { IoIosArrowDown } from "react-icons/io";
-import { IoIosArrowUp } from "react-icons/io";
 import Footer from "../components/footer";
-
+import SurveyTargetBoxSmall from "../components/surveyTarget/surveyTargetBoxSmall";
+import SurveyTargetBoxBig from "../components/surveyTarget/surveyTargetBoxBig";
 const SurveyList = () => {
-  const [selectBoxOpen, setSelectBoxOpen] = useState(false);
   const [surveyList, setSurveyList] = useState(0);
+  const [surveyTargetBox, setSurveyTargetBox] = useState(0);
 
   const surveyListArr = [
     { name: "전체" },
@@ -26,20 +25,19 @@ const SurveyList = () => {
     setSurveyList(index);
   };
 
-  const handleSelectBox = () => {
-    setSelectBoxOpen(!selectBoxOpen);
+  const handleSurveyTarget = () => {
+    if (surveyTargetBox === 0) {
+      setSurveyTargetBox(1);
+    } else if (surveyTargetBox === 1) {
+      setSurveyTargetBox(0);
+    }
   };
 
-  const targetAge = [
-    "나이 상관없음",
-    "10대",
-    "20대",
-    "30대",
-    "40대",
-    "50대",
-    "60대 이상",
-  ];
-  const targetGender = ["성별 상관없음", "남성", "여성"];
+  const surveyTargetObj = {
+    0: <SurveyTargetBoxSmall handleSurveyTarget={handleSurveyTarget} />,
+    1: <SurveyTargetBoxBig handleSurveyTarget={handleSurveyTarget} />,
+  };
+
   return (
     <>
       <Navbar />
@@ -65,56 +63,8 @@ const SurveyList = () => {
         </div>
         <div className="surveyListRight">
           <div className="surveyListRightTop">
-            <div className="surveyListSelectBoxSmall" onClick={handleSelectBox}>
-              <div className="surveyListSelectBoxTitleAndDetail">
-                <div className="surveyListSelectBoxTitle">
-                  설문조사 대상 선택
-                </div>
-                <div className="surveyListSelectBoxDetail">
-                  20대, 여성 선택됨
-                </div>
-              </div>
-              <IoIosArrowDown
-                size="20px"
-                color="#8E8E8E"
-                className="surveyListSelectBoxArrowDown"
-              />
-            </div>
+            {surveyTargetObj[surveyTargetBox]}
             <button className="surveyWriteBnt">설문 작성하기</button>
-          </div>
-
-          <div className="surveyListSelectBoxBig" onClick={handleSelectBox}>
-            <div className="surveyListSelectBoxBigTop">
-              <div className="surveyListSelectBoxTitleAndDetail">
-                <div className="surveyListSelectBoxTitle">
-                  설문조사 대상 선택
-                </div>
-                <div className="surveyListSelectBoxDetail">
-                  클릭하신 조건의 참가자가 참여할 수 있는 설문만 보여드립니다.
-                </div>
-              </div>
-              <IoIosArrowUp
-                size="20px"
-                color="#8E8E8E"
-                className="surveyListSelectBoxArrowUp"
-              />
-            </div>
-            <div
-              className="surveyListSelectBoxBigTagContainer"
-              onClick={handleSelectBox}
-            >
-              <div className="surveyListTargetAge">
-                {targetAge.map((age) => (
-                  <span className="surveyListTargetTag">{age}</span>
-                ))}
-              </div>
-              <div className="surveyListTargetGender">
-                {targetGender.map((gender) => (
-                  <span className="surveyListTargetTag">{gender}</span>
-                ))}
-              </div>
-              <button className="surveyListTargetBnt">적용</button>
-            </div>
           </div>
         </div>
       </div>
