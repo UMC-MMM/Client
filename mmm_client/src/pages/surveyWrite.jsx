c268f070d966199103661276d4ad954d81998cd1
junwon1997 <junwon20170339@gmail.com>
2022-08-17 20:38:42 +0900

    설문조사 게시화면 절반 완성

--- mmm_client/src/pages/surveyWrite.jsx
+++ mmm_client/src/pages/surveyWrite.jsx
@@ -1,7 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "../components/navbar";
+import SingleSelection from "../components/surveyPostKindContents/singleSelection";
+import MultipleSelection from "../components/surveyPostKindContents/multipleSelection";
+import DescriptiveForm from "../components/surveyPostKindContents/descriptiveForm";
 
 const SurveyWrite = () => {
+  const [surveyList, setSurveyList] = useState("단일선택형");
+  const [surveyNumber, setSurveyNumber] = useState(0);
+
+  const onSurveyListChange = (e) => {
+    const { value } = e.target;
+    setSurveyList(value);
+  };
+
+  const handleSurveyNumber = () => {
+    setSurveyNumber();
+  };
+
+  const surveyLists = [
+    { id: "0", value: "단일선택형" },
+    { id: "1", value: "다중선택형" },
+    { id: "2", value: "서술형" },
+  ];
+
+  const surveyListsObj = {
+    0: <SingleSelection />,
+    1: <MultipleSelection />,
+    2: <DescriptiveForm />,
+  };
+
   return (
     <>
       <Navbar />
@@ -23,6 +50,42 @@ const SurveyWrite = () => {
           />
         </div>
       </div>
+      <div className="surveyPostTitleBox">
+        <div className="surveyPostTitleinputAndChooseSurveyKindAndDelete">
+          <div className="surveyPostTitleInput">
+            <input
+              id="surveyPostTitleInputId"
+              type="text"
+              name="id"
+              placeholder="질문을 입력하세요."
+            />
+          </div>
+          <div className="surveyPostChooseSurveyKindAndDelete">
+            <div className="surveyPostChooseSurveyKind">
+              <select
+                className="surveyPostChooseSurveyKindSelect"
+                value={surveyList}
+                onChange={onSurveyListChange}
+              >
+                {surveyLists.map((surveyList) => (
+                  <option
+                    value={surveyList.id}
+                    onChange={() => handleSurveyNumber(surveyList.id)}
+                  >
+                    {surveyList.value}
+                  </option>
+                ))}
+              </select>
+            </div>
+            <div className="surveyPostDelete">삭제</div>
+          </div>
+        </div>
+        <div className="surveyPostkindContent">
+          {surveyListsObj[surveyNumber]}
+        </div>
+      </div>
+      <div className="surveyPostPlusQuestionsBtn">+질문추가</div>
+      <div className="surveyPostCompleteBtn">설문작성완료</div>
     </>
   );
 };
