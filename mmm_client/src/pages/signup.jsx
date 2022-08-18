91cddeeabc5b86d86874a67625475b027811e822
seyxxn <lucy9480@gmail.com>
2022-08-18 11:25:10 +0900

    회원가입 페이지에 form 태그 추가

--- mmm_client/src/pages/signup.jsx
+++ mmm_client/src/pages/signup.jsx
@@ -23,91 +23,93 @@ const Signup = () => {
       <Link to="/login" style={{ textDecoration: "none", cursor: "pointer" }}>
         <div className="signUpLoginBtn">로그인 하기</div>
       </Link>
-      <div className="signupSignup">회원가입</div>
-      <div className="signupBoxContainer">
-        <div className="signupBoxTitle">이름</div>
-        <div className="signupInputName">
-          <input
-            id="signupInputName"
-            type="text"
-            name="name"
-            placeholder="실명을 입력하세요."
-          ></input>
-        </div>
-        <div className="signupBoxTitle">이메일</div>
-        <div className="signupInputEmail">
-          <input
-            id="signupInputEmail"
-            type="text"
-            name="email"
-            placeholder="이메일 주소를 입력하세요."
-          ></input>
-        </div>
-        <div className="signupBoxTitle">아이디</div>
-        <div className="signupInputId">
-          <input
-            id="signupInputId"
-            type="text"
-            name="id"
-            placeholder="서베이브에 사용할 아이디를 입력하세요."
-          ></input>
-        </div>
-        <div className="signupBoxTitle">비밀번호</div>
-        <div className="signupInputPassword">
-          <input
-            id="signupInputPassword"
-            type="password"
-            name="password"
-            placeholder="6~16자의 영문자, 숫자, 특수문자를 사용하세요."
-          ></input>
-        </div>
-        <div className="signupBoxTitle">비밀번호 확인</div>
-        {/* <div className="signupBoxError">오류메세지</div> */}
-        <div className="signupInputPasswordCheck">
-          <input
-            id="signupInputPasswordCheck"
-            type="text"
-            name="passwordCheck"
-            placeholder="비밀번호를 한번 더 입력하세요."
-          ></input>
-        </div>
-        <div className="signupAgreeContainer" onClick={handleAgree}>
-          <div
-            className="signupAgree"
-            onClick={handleAgree}
-            style={{ color: color }}
-          >
-            서베이브의 개인정보 수집 및 이용에 동의합니다.
+      <form>
+        <div className="signupSignup">회원가입</div>
+        <div className="signupBoxContainer">
+          <div className="signupBoxTitle">이름</div>
+          <div className="signupInputName">
+            <input
+              id="signupInputName"
+              type="text"
+              name="name"
+              placeholder="실명을 입력하세요."
+            ></input>
           </div>
-          <BsCheckCircleFill
-            className="signupAgreeIcon"
-            size="28px"
-            color={color}
-            onClick={handleAgree}
-          />
-        </div>
-        <Link
-          to="/agreement"
-          style={{ textDecoration: "none", cursor: "pointer" }}
-        >
-          <div className="signupAgreeCheck">동의서 확인하기 →</div>
-        </Link>
+          <div className="signupBoxTitle">이메일</div>
+          <div className="signupInputEmail">
+            <input
+              id="signupInputEmail"
+              type="text"
+              name="email"
+              placeholder="이메일 주소를 입력하세요."
+            ></input>
+          </div>
+          <div className="signupBoxTitle">아이디</div>
+          <div className="signupInputId">
+            <input
+              id="signupInputId"
+              type="text"
+              name="id"
+              placeholder="서베이브에 사용할 아이디를 입력하세요."
+            ></input>
+          </div>
+          <div className="signupBoxTitle">비밀번호</div>
+          <div className="signupInputPassword">
+            <input
+              id="signupInputPassword"
+              type="password"
+              name="password"
+              placeholder="6~16자의 영문자, 숫자, 특수문자를 사용하세요."
+            ></input>
+          </div>
+          <div className="signupBoxTitle">비밀번호 확인</div>
+          {/* <div className="signupBoxError">오류메세지</div> */}
+          <div className="signupInputPasswordCheck">
+            <input
+              id="signupInputPasswordCheck"
+              type="text"
+              name="passwordCheck"
+              placeholder="비밀번호를 한번 더 입력하세요."
+            ></input>
+          </div>
+          <div className="signupAgreeContainer" onClick={handleAgree}>
+            <div
+              className="signupAgree"
+              onClick={handleAgree}
+              style={{ color: color }}
+            >
+              서베이브의 개인정보 수집 및 이용에 동의합니다.
+            </div>
+            <BsCheckCircleFill
+              className="signupAgreeIcon"
+              size="28px"
+              color={color}
+              onClick={handleAgree}
+            />
+          </div>
+          <Link
+            to="/agreement"
+            style={{ textDecoration: "none", cursor: "pointer" }}
+          >
+            <div className="signupAgreeCheck">동의서 확인하기 →</div>
+          </Link>
 
-        <div className="signupSubmitBtn">
-          <input
-            id="signupSubmitBtn"
-            className="signupSubmit"
-            type="submit"
-            name="submit"
-            value="회원가입 하기"
-          />
-        </div>
-        {/* <div className="loginFindIdandPassword findInSignup">
+          <div className="signupSubmitBtn">
+            <input
+              id="signupSubmitBtn"
+              className="signupSubmit"
+              type="submit"
+              name="submit"
+              value="회원가입 하기"
+            />
+          </div>
+          {/* <div className="loginFindIdandPassword findInSignup">
           <span>아이디 찾기</span>
           <span>|</span>
           <span>비밀번호 찾기</span>
         </div> */}
-      </div>
+        </div>
+      </form>
     </>
   );
 };
